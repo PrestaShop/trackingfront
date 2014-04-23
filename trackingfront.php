156a778dcb4bb276460c2a746cbcfaea38a31472
Emilien Puget <puget.e@gmail.com>
2014-04-23 18:16:01 +0200

    // ++version

--- trackingfront.php
+++ trackingfront.php
@@ -33,7 +33,7 @@ class TrackingFront extends Module
 	{
 		$this->name = 'trackingfront';
 		$this->tab = 'shipping_logistics';
-		$this->version = '1.5';
+		$this->version = '1.6';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
