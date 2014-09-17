8554f99dcef526a922e56acdd1b0b4d65b476777
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2014-09-17 12:00:43 +0200

    // Version updated to v1.8.2

--- trackingfront.php
+++ trackingfront.php
@@ -33,7 +33,7 @@ class TrackingFront extends Module
 	{
 		$this->name = 'trackingfront';
 		$this->tab = 'shipping_logistics';
-		$this->version = '1.8.1';
+		$this->version = '1.8.2';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
